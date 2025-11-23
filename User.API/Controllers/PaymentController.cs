81763043f0937bb753dc91e4abdad3a5e5996c05
JASSADHAMMI <jassadhammi@gmail.com>
2025-11-23 20:37:28 +0530

    bug fixed

--- User.API/Controllers/PaymentController.cs
+++ User.API/Controllers/PaymentController.cs
@@ -7,32 +7,35 @@ using Stripe.Checkout;
 public class PaymentController : ControllerBase
 {
     [HttpPost("create-checkout-session")]
-    public IActionResult CreateCheckoutSession()
+    public IActionResult CreateCheckoutSession([FromBody] StripeCheckoutRequest req)
     {
         StripeConfiguration.ApiKey = "sk_test_51SSAQMBOxzp7UNACemUl8X68qvF8bywcPV87XnqZ9ssbQfcHQaQpquwNslptpjWCd6fOsU726IwIGsUVdFP0axBg00kpqo6W1l";
+
+        // Your website domain
+        var domain = "https://localhost:7291";
 
         var options = new SessionCreateOptions
         {
             PaymentMethodTypes = new List<string> { "card" },
-            Mode = "payment",
-            SuccessUrl = "https://localhost:7175/user_orders",
-            CancelUrl = "https://localhost:7175/cancel",
             LineItems = new List<SessionLineItemOptions>
         {
             new SessionLineItemOptions
             {
                 PriceData = new SessionLineItemPriceDataOptions
                 {
-                    Currency = "usd",
-                    UnitAmount = 1000,
+                    UnitAmount = (long)(req.Amount * 100),  // ₹ → paise
+                    Currency = "inr",                       // IMPORTANT
                     ProductData = new SessionLineItemPriceDataProductDataOptions
                     {
-                        Name = "Test Product"
+                        Name = req.ProductName
                     }
                 },
                 Quantity = 1
             }
-        }
+        },
+            Mode = "payment",
+            SuccessUrl = $"{domain}/user_orders",
+            CancelUrl = $"{domain}/cancel"
         };
 
         var service = new SessionService();
@@ -40,6 +43,41 @@ public class PaymentController : ControllerBase
 
         return Ok(new { url = session.Url });
     }
+
+    //[HttpPost("create-checkout-session")]
+    //public IActionResult CreateCheckoutSession()
+    //{
+    //    StripeConfiguration.ApiKey = "sk_test_51SSAQMBOxzp7UNACemUl8X68qvF8bywcPV87XnqZ9ssbQfcHQaQpquwNslptpjWCd6fOsU726IwIGsUVdFP0axBg00kpqo6W1l";
+
+    //    var options = new SessionCreateOptions
+    //    {
+    //        PaymentMethodTypes = new List<string> { "card" },
+    //        Mode = "payment",
+    //        SuccessUrl = "https://localhost:7291/user_orders",
+    //        CancelUrl = "https://localhost:7291/cancel",
+    //        LineItems = new List<SessionLineItemOptions>
+    //    {
+    //        new SessionLineItemOptions
+    //        {
+    //            PriceData = new SessionLineItemPriceDataOptions
+    //            {
+    //                Currency = "usd",
+    //                UnitAmount = 1000,
+    //                ProductData = new SessionLineItemPriceDataProductDataOptions
+    //                {
+    //                    Name = "Test Product"
+    //                }
+    //            },
+    //            Quantity = 1
+    //        }
+    //    }
+    //    };
+
+    //    var service = new SessionService();
+    //    var session = service.Create(options);
+
+    //    return Ok(new { url = session.Url });
+    //}
     //    [HttpPost("create-checkout-session")]
     //    public IActionResult CreateCheckoutSession([FromBody] StripeCheckoutRequest req)
     //    {
